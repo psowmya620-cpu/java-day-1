ffa238a18e89fcd68cf92cfcb85835ac380a2afd
psowmya620-cpu <psowmya620@gmail.com>
2026-02-07 11:02:05 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+class Student{
+    public static void main(String[]args)
+    {
+        String name="sowmya";
+        Byte age=20;
+        char grade='A';
+        Short marks=100;
+        String status="pass";
+        System.out.println("Sring name"+name);
+        System.out.println("Byte age"+age);
+        System.out.println("char grade"+grade);
+        System.out.println("short marks"+marks);
+        System.out.println("String status"+status);
+ }
+}
